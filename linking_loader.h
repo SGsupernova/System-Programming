84842634d3509283a24ad52a5318e9ecfea9eafd
Lee Tae Seung <dlxotmd125@gmail.com>
2016-04-24 00:57:20 +0900

    modify

--- linking_loader.h
+++ linking_loader.h
@@ -39,7 +39,7 @@ int command_bp (struct bpLink ** bpLinkHead_ptr, const char * inputStr);
 
 // return error_flag
 int linking_loader_pass1(int progaddr, int argc, char *object_filename[], ESTAB extern_symbol_table[]);
-int linking_loader_pass2();
+int linking_loader_pass2 (int progaddr, int argc, char * object_filename[], ESTAB extern_symbol_table[]);
 
 int linking_loader_search_estab_symbol (ESTAB extern_symbol_table[], int argc, char * str, int * address);
 void linking_loader_enter_symbol (struct __extern_symbol **extern_symbol, char * str, int address);
